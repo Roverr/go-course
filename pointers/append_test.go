66c1c77a6ca2269b4e4dc2ec3c9f63ce417a64d6
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2018-03-01 15:57:41 +0100

    feat(pointers): Cool benchmark example for appending

---
+++ pointers/append_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+// Code from https://stackoverflow.com/a/37621532
+type MyStruct struct {
+	F1, F2, F3, F4, F5, F6, F7 string
+	I1, I2, I3, I4, I5, I6, I7 int64
+}
+
+func BenchmarkAppendingStructs(b *testing.B) {
+	var s []MyStruct
+
+	for i := 0; i < b.N; i++ {
+		s = append(s, MyStruct{})
+	}
+}
+
+func BenchmarkAppendingPointers(b *testing.B) {
+	var s []*MyStruct
+
+	for i := 0; i < b.N; i++ {
+		s = append(s, &MyStruct{})
+	}
+}
